e690ff133a40be45124e659549b0c93a7f971159
sumsumcity <raffi_waespi@hotmail.com>
2021-05-03 20:13:44 +0200

    first commit

---
+++ src/main/java/com/jasofalcon/message/Message.java
@@ -0,0 +1,39 @@
+package com.jasofalcon.message;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.jasofalcon.message.MessageType;
+import com.jasofalcon.user.User;
+
+import java.io.Serializable;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Message implements Serializable{
+
+    private User user;
+    private MessageType type;
+    private String data;
+
+    public MessageType getType() {
+        return type;
+    }
+
+    public void setType(MessageType type) {
+        this.type = type;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getData(){
+        return data;
+    }
+}
